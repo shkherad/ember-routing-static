4f1b2d73643520ca749b0612020e59e04af0c934
Jeffrey Horn <jeffh@generalassemb.ly>
2016-03-22 10:42:58 -0400

    Demo for WDI 010 on Tuesday, March 22, 2016

--- app/router.js
+++ app/router.js
@@ -6,6 +6,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function () {
+  this.route('team', function () {
+    this.route('engineering');
+    this.route('leadership');
+  });
+  this.route('contact', function () {
+    this.route('boston');
+    this.route('nyc');
+  });
+  this.route('about');
 });
 
 export default Router;
