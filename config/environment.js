cacca5a91b6c3fb71e22b038e14ffdc4b04c2874
laurenfazah <laurenfazah@gmail.com>
2016-05-26 14:12:57 -0400

    Removed all references to ember-template
    Updated to ember-routing-static

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ember-template',
+    modulePrefix: 'ember-routing-static',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
