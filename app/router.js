6e000d665898d71d4a4458817ceb972e379a4973
berziiii <brian@offpistedesigns.com>
2016-07-18 15:15:43 -0400

    solution for 012 master training

--- app/router.js
+++ app/router.js
@@ -6,6 +6,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function () {
+  this.route('about');
+  this.route('team', function() {
+    this.route('leadership');
+    this.route('engineering');
+  });
+  this.route('contact', function() {
+    this.route('boston');
+    this.route('nyc');
+  });
 });
 
 export default Router;
