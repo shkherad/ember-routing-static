2dcdf9322ea377e383f7ae60af90166a3a250924
shkherad <shkherad@bu.edu>
2016-07-26 11:04:05 -0400

    class notes added
    class notes added

--- app/router.js
+++ app/router.js
@@ -6,6 +6,16 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function () {
+  this.route('about', {path: '/about_shireen'});
+  this.route('team', function(){
+    this.route('engineering');
+    this.route('leadership');
+  });
+  this.route('contact', function() {
+    this.route('boston');
+    this.route('nyc');
+  });
+  this.route('shireen');
 });
 
 export default Router;
